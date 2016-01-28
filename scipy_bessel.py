adb010a55b0d7b1925823b7b69a7551e8bbb9277
AndrewWUw <andrew_wu@outlook.com>
2016-01-28 17:20:46 +1100

    import new ipynb files

---
+++ scipy_bessel.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy import stats
+from scipy import io as spio
+from scipy import misc
+from scipy import special
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import pandas
+
+def drumhead_height(n, k, distance, angle, t):
+   kth_zero = special.jn_zeros(n, k)[-1]
+   return np.cos(t) * np.cos(n*angle) * special.jn(n, distance*kth_zero)
+   
+theta = np.r_[0:2*np.pi:50j]
+radius = np.r_[0:1:50j]
+x = np.array([r * np.cos(theta) for r in radius])
+y = np.array([r * np.sin(theta) for r in radius])
+z = np.array([drumhead_height(1, 1, r, theta, 0.5) for r in radius])
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap=cm.jet)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.show()
